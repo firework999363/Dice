ee1336adc0cca9d89f2c7032736b6cbe4e115c08
firework999363 <firework999363@gmail.com>
2015-09-20 23:47:14 -0700

    basics complete

--- Dice.pde
+++ Dice.pde
@@ -1,18 +1,38 @@
-int colors = 255;
+int colorR;
+int colorG;
+int colorB;
+int s = 6;
+int sum;
 void setup()
 {
-	size(500,500);
+	background(0);
+	size(470,520);
 	noLoop();
-	
+	noStroke();
+	frameRate(2);
 }
 void draw()
 {
-	Die bob = new Die(0,0);
-	bob.roll();
-	bob.show();
+	sum=0;
+	for(int y=10;y<=450;y=y+46)
+	{
+		for(int x=10;x<=450;x=x+46)
+		{
+			colorR = (int)(Math.random()*220)+36;
+			colorG = (int)(Math.random()*220)+36;
+			colorB = (int)(Math.random()*220)+36;
+			Die bob = new Die(x,y);
+			bob.roll();
+			bob.show();
+		}
+	}
+	fill(255);
+	textSize(24);
+	text("TOTAL: " + sum,170,500);
 }
 void mousePressed()
 {
+	background(0);
 	redraw();
 }
 class Die 
@@ -26,60 +46,60 @@ class Die
 	}
 	void roll()
 	{
-		
-
-	}
-	void show()
-	{
 		if (number==1)
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
-			ellipse(myX+18,myY+18,2,2);
+			ellipse(myX+18,myY+18,s,s);
+			sum= sum +1;
 		}
 		else if (number==2)
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
-			ellipse(myX+12,myY+18,2,2);
-			ellipse(myX+24,myY+18,2,2);
+			ellipse(myX+12,myY+18,s,s);
+			ellipse(myX+24,myY+18,s,s);
+			sum= sum +2;
 		}
 		else if (number==3)
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
-			ellipse(myX+9,myY+9,2,2);
-			ellipse(myX+18, myY+18, 2, 2);
-			ellipse(myX+27, myY+27, 2, 2);
+			ellipse(myX+9,myY+9,s,s);
+			ellipse(myX+18, myY+18, s, s);
+			ellipse(myX+27, myY+27, s, s);
+			sum= sum +3;
 		}
 		else if (number==4)
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
-			ellipse(myX+12,myY+12,2,2);
-			ellipse(myX+24,myY+12,2,2);
-			ellipse(myX+12,myY+24,2,2);
-			ellipse(myX+24,myY+24,2,2);
+			ellipse(myX+12,myY+12,s,s);
+			ellipse(myX+24,myY+12,s,s);
+			ellipse(myX+12,myY+24,s,s);
+			ellipse(myX+24,myY+24,s,s);
+			sum= sum +4;
 		}
 		else if (number==5)
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
-			ellipse(myX+9,myY+9,2,2);
-			ellipse(myX+9,myY+27,2,2);
+			ellipse(myX+9,myY+9,s,s);
+			ellipse(myX+9,myY+27,s,s);
 			ellipse(myX+18,myY+18,s,s);
 			ellipse(myX+27,myY+9,s,s);
 			ellipse(myX+27,myY+27,s,s);
+			sum= sum +5;
 		}
 		else 
 		{
-			fill(colors);
-			rect(myX,myY,36,36);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
 			fill(0);
 			ellipse(myX+12,myY+9,s,s);
 			ellipse(myX+12,myY+18,s,s);
@@ -87,6 +107,10 @@ class Die
 			ellipse(myX+24,myY+9,s,s);
 			ellipse(myX+24,myY+18,s,s);
 			ellipse(myX+24,myY+27,s,s);
+			sum= sum +6;
 		}
+	}
+	void show()
+	{
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -14,20 +14,41 @@ import java.io.IOException;
 
 public class Dice extends PApplet {
 
+int colorR;
+int colorG;
+int colorB;
+int s = 6;
+int sum;
 public void setup()
 {
-	size(500,500);
+	background(0);
+	size(470,520);
 	noLoop();
-	
+	noStroke();
+	frameRate(2);
 }
 public void draw()
 {
-	Die bob = new Die(0,0);
-	bob.roll();
-	bob.show();
+	sum=0;
+	for(int y=10;y<=450;y=y+46)
+	{
+		for(int x=10;x<=450;x=x+46)
+		{
+			colorR = (int)(Math.random()*220)+36;
+			colorG = (int)(Math.random()*220)+36;
+			colorB = (int)(Math.random()*220)+36;
+			Die bob = new Die(x,y);
+			bob.roll();
+			bob.show();
+		}
+	}
+	fill(255);
+	textSize(24);
+	text("TOTAL: " + sum,170,500);
 }
 public void mousePressed()
 {
+	background(0);
 	redraw();
 }
 class Die 
@@ -41,56 +62,72 @@ class Die
 	}
 	public void roll()
 	{
-		
-
-	}
-	public void show()
-	{
 		if (number==1)
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+18,myY+18,2,2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+18,myY+18,s,s);
+			sum= sum +1;
 		}
 		else if (number==2)
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+12,myY+18,2,2);
-			ellipse(myX+24,myY+18,2,2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+12,myY+18,s,s);
+			ellipse(myX+24,myY+18,s,s);
+			sum= sum +2;
 		}
 		else if (number==3)
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+9,myY+9,2,2);
-			ellipse(myX+18, myY+18, 2, 2);
-			ellipse(myX+27, myY+27, 2, 2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+9,myY+9,s,s);
+			ellipse(myX+18, myY+18, s, s);
+			ellipse(myX+27, myY+27, s, s);
+			sum= sum +3;
 		}
 		else if (number==4)
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+12,myY+12,2,2);
-			ellipse(myX+24,myY+12,2,2);
-			ellipse(myX+12,myY+24,2,2);
-			ellipse(myX+24,myY+24,2,2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+12,myY+12,s,s);
+			ellipse(myX+24,myY+12,s,s);
+			ellipse(myX+12,myY+24,s,s);
+			ellipse(myX+24,myY+24,s,s);
+			sum= sum +4;
 		}
 		else if (number==5)
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+9,myY+9,2,2);
-			ellipse(myX+9,myY+27,2,2);
-			ellipse(myX+18,myY+18,2,2);
-			ellipse(myX+27,myY+9,2,2);
-			ellipse(myX+27,myY+27,2,2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+9,myY+9,s,s);
+			ellipse(myX+9,myY+27,s,s);
+			ellipse(myX+18,myY+18,s,s);
+			ellipse(myX+27,myY+9,s,s);
+			ellipse(myX+27,myY+27,s,s);
+			sum= sum +5;
 		}
 		else 
 		{
-			rect(myX,myY,36,36);
-			ellipse(myX+12,myY+9,2,2);
-			ellipse(myX+12,myY+18,2,2);
-			ellipse(myX+12,myY+27,2,2);
-			ellipse(myX+24,myY+9,2,2);
-			ellipse(myX+24,myY+18,2,2);
-			ellipse(myX+24,myY+27,2,2);
+			fill(colorR,colorG,colorB);
+			rect(myX,myY,36,36,5);
+			fill(0);
+			ellipse(myX+12,myY+9,s,s);
+			ellipse(myX+12,myY+18,s,s);
+			ellipse(myX+12,myY+27,s,s);
+			ellipse(myX+24,myY+9,s,s);
+			ellipse(myX+24,myY+18,s,s);
+			ellipse(myX+24,myY+27,s,s);
+			sum= sum +6;
 		}
+	}
+	public void show()
+	{
 	}
 }
   static public void main(String[] passedArgs) {
